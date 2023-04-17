25879fded3d649d079af17b1c002a04d5cc28d3f
Jordan <jordanakehurst@gmail.com>
2023-04-17 18:20:25 +0100

    Project Load/Play Fix
    Project and levels have been tested and can confirm are working.
    
    Currently no more issues with project loading/scripts not working :)

--- KrakScroll/Assets/Scripts/enemyChase.cs
+++ KrakScroll/Assets/Scripts/enemyChase.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enemyChase : MonoBehaviour
+public class EnemyChase : MonoBehaviour
 {
     public ObstacleBats[] enemyArray;
 
--- KrakScroll/Assets/Scripts/playerMovement.cs
+++ KrakScroll/Assets/Scripts/playerMovement.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     [Header("Adjustables")] // ADDS TITLE IN INSPECTOR - ZERO EFFECTS ON CODE
     public float speed;
